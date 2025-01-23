5a52c5f865429a4c154042f44f5626ee97785579
shakti2505 <kashyapshakti1994@gmail.com>
2025-01-23 12:38:55 +0530

    Error fixed with protected routes

--- src/utils/ProtectedRoute.tsx
+++ src/utils/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 // import { useUser } from "./criticalState";
 
 interface ProtectedRouteProps {
@@ -7,9 +7,10 @@ interface ProtectedRouteProps {
 }
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
   const storedUser = localStorage.getItem("user");
+  const navigate = useNavigate();
   const user = storedUser ? JSON.parse(storedUser) : null;
   if (!user) {
-    return <Navigate to="/" />;
+    navigate("/");
   }
 
   return <>{children}</>;
