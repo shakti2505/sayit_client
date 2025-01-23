61ea3ba9d1396ab8aab9a94c35a02f84bfa60aed
shakti2505 <kashyapshakti1994@gmail.com>
2025-01-23 14:36:32 +0530

    integrated hash router

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
