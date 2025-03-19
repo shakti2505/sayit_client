8f35b1503baac8909211c9ffa4baeee372998ff0
shakti2505 <kashyapshakti1994@gmail.com>
2025-03-19 20:44:35 +0530

    refactor: comment out unused imports and code in groupChatSelector

--- src/components/chats/slices/selector/groupChatSelector.ts
+++ src/components/chats/slices/selector/groupChatSelector.ts
@@ -1,22 +1,22 @@
-import { createSelector } from "@reduxjs/toolkit";
-import { RootState } from "../../../../store/rootreducer";
-import { groupChats } from "../types/groupMessagesTypes";
+// import { createSelector } from "@reduxjs/toolkit";
+// import { RootState } from "../../../../store/rootreducer";
+// import { groupChats } from "../types/groupMessagesTypes";
 
-const selectEncryptedMessages = (state: RootState) =>
-  state.getGroupChatState.groupChats;
+// const selectEncryptedMessages = (state: RootState) =>
+//   state.getGroupChatState.groupChats;
 
 
-// memoized the selector only to decrypt the messages only when changes
-export const SelectDecryptedMessages = createSelector(
-  [selectEncryptedMessages], // input selector
-  (encryptedMessages) => {
-    console.log("encrypting messages.....");
-    return encryptedMessages.map((date: groupChats) =>
-      date.messages.map((msg) => ({
-        ...msg,
-        /// decrypte the message content here using decryption method
-      }))
-    );
-  }
-);
+// // memoized the selector only to decrypt the messages only when changes
+// export const SelectDecryptedMessages = createSelector(
+//   [selectEncryptedMessages], // input selector
+//   (encryptedMessages) => {
+//     console.log("encrypting messages.....");
+//     return encryptedMessages.map((date: groupChats) =>
+//       date.messages.map((msg) => ({
+//         ...msg,
+//         /// decrypte the message content here using decryption method
+//       }))
+//     );
+//   }
+// );
 
